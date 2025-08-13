03abe3f88db6b10d210277281cbd21d1b18e02b0
AmartyaDev007 <AmartyaDev007@users.noreply.github.com>
2025-08-13 11:30:40 +0530

    add basic functionality and logic to gameboard, so when someone clicks on the gameboard it's actually interactable

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # basic-tictactoe
+
+simple Tic Tac Toe game without 
+
+Exercise by [odin project](https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe)
---
+++ code.js
@@ -0,0 +1,103 @@
+
+
+const gameInteraction = (function () { 
+    let currentChar = "X";
+    
+
+    const gameBoard = document.querySelector(".game-board");
+    
+    const switcher = () => { 
+        if (currentChar == "X") { 
+            currentChar = "O";
+        } else { 
+            currentChar = "X";
+        }
+    }
+
+
+    gameBoard.addEventListener("click", (e) => { 
+        if (e.target.textContent == "") { 
+            e.target.textContent = currentChar;
+            switcher(currentChar);
+        }
+    })
+
+
+})()
+
+
+
+
+let boardItem = { 
+    11: "",
+    12: "",
+    13: "",
+    21: "",
+    22: "",
+    23: "",
+    31: "",
+    32: "",
+    33: "",
+}
+
+let player1 = "X";
+let player2 = "O";
+
+// if (boardItem[11] == boardItem[12] == boardItem[13] ||
+//     boardItem[21] == boardItem[22] == boardItem[23] ||
+//     boardItem[31] == boardItem[32] == boardItem[33] ||
+//     boardItem[11] == boardItem[21] == boardItem[31] ||
+//     boardItem[12] == boardItem[22] == boardItem[32] ||
+//     boardItem[13] == boardItem[23] == boardItem[33] ||
+//     boardItem[11] == boardItem[22] == boardItem[33] ||
+//     boardItem[13] == boardItem[22] == boardItem[31]) {
+    
+
+// }
+
+
+
+// method for gameboard (manages eventListener for each cell and calls other methods)
+
+// const gameBoard = (function () { 
+    
+// })();
+
+// method for managing player scores (adding score)
+
+// method that checks for a win after every button press
+
+// method to reset game board and scores 
+
+
+
+const playerManager = (function() { 
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
+    function addScore(player) { 
+        if (player == "X") { 
+            playerOneScore++;
+        } else { 
+            playerTwoScore++;
+        }
+    }
+
+    const resetScore = () => { 
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+
+
+    return { 
+        addScore,
+        getPlayerOneScore: () => playerOneScore,
+        getPlayerTwoScore: () => playerTwoScore,
+        resetScore,
+    }
+
+    
+})() 
+
+
+
