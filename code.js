cb9ae491e77c81aabe632da6bfe17da4aba5035d
AmartyaDev007 <AmartyaDev007@users.noreply.github.com>
2025-08-28 01:29:48 +0530

    logic section completed with additions for handling draws, wins, display content etc.

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ let boardItem = {
         11: "", 12: "", 13: "",
         21: "", 22: "", 23: "",
         31: "", 32: "", 33: "",
-    }   
+}   
 
 const startInteraction = (function() { 
     let playerOneName = "";
@@ -10,36 +10,46 @@ const startInteraction = (function() {
 
     document.querySelector(".starter-form").addEventListener("submit", (e) => { 
         e.preventDefault();
-
-        playerOneName = document.querySelector("#player-1-name").value;
-        playerTwoName = document.querySelector("#player-2-name").value;
+        const playerOneInput = document.querySelector("#player-1-name");
+        const playerTwoInput = document.querySelector("#player-2-name");
+        playerOneName = playerOneInput.value;
+        playerTwoName = playerTwoInput.value;
 
         if (playerOneName === "" || playerTwoName === "") { 
             return;
         }
+
+        playerOneInput.value = "";
+        playerTwoInput.value = "";
         
         document.querySelector(".starter-section").style.display = "none";
         document.querySelector(".game-section").style.display = "block";
+
+        document.querySelector(".player-name-one-display").textContent = "Player " + playerOneName + " (X)";
+        document.querySelector(".player-name-two-display").textContent = "Player" + playerTwoName + " (O)";
+        document.querySelector(".player-turn-display").textContent = playerOneName + "'s Turn!";
     });
 
     return { 
         getPlayerOneName: () => playerOneName,
         getPlayerTwoName: () => playerTwoName,
+
+        setPlayerOneName: (name) => playerOneName = name,
+        setPlayerTwoName: (name) => playerTwoName = name,
     }
 })();
 
-
 const gameInteraction = (function () { 
     let currentChar = "X";
-    
-
     const gameBoard = document.querySelector(".game-board");
     
     const switcher = () => { 
         if (currentChar == "X") { 
             currentChar = "O";
+            document.querySelector(".player-turn-display").textContent = startInteraction.getPlayerTwoName() + "'s turn";
         } else { 
             currentChar = "X";
+            document.querySelector(".player-turn-display").textContent = startInteraction.getPlayerOneName() + "'s turn";
         }
     }
 
@@ -51,22 +61,29 @@ const gameInteraction = (function () {
 
         boardItem[e.target.id] = currentChar;
         // call the winner handler for checking :) 
-
         if (someoneWon()) {
             // first find who won by finding the currentChar and the player associated 
             // declare a winner with a popup dialog with "Play again" button and "restart button"
             switcher(currentChar);
 
             winnerHandler(currentChar);
+            
+        } else if (drawCheck()) { 
+            drawHandler();
         }
     })
-
-
 })()
 
+const drawCheck = function() {
+    for (let key of Object.keys(boardItem)) {
+        if (boardItem[key] === "") {
+            return false; // exit the whole function
+        }
+    }
+    return true;
+};
 
 const someoneWon = function () { 
-
     const b = boardItem; 
 
     function isWinningLine(a, b, c) {
@@ -85,21 +102,65 @@ const someoneWon = function () {
     ) {
         return true;
     }
-
     return false;
-
 };
 
-function winnerHandler() { 
-    
+const startRestartBtnHandlers = (function() { 
+    function resetGameBoardObject() { 
+        Object.keys(boardItem).forEach(key => {
+            boardItem[key] = "";
+        });
+    }
+
+    function resetPlayerNames() { 
+        startInteraction.setPlayerOneName("");
+        startInteraction.setPlayerTwoName("");
+    }
+
+    function resetGameBoardCellContent() { 
+        const cells = document.querySelectorAll(".game-board .cell");
+
+        cells.forEach(cell => {
+            cell.textContent = "";
+        });
+    }
+
+    const restartBtns = document.querySelectorAll(".restart-btn");
+    const replay = document.querySelector(".replay-btn");
+
+    restartBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => { 
+            resetGameBoardObject();
+            resetPlayerNames();
+            resetGameBoardCellContent();
+            document.querySelector("#end-dialog").close();
+            document.querySelector(".game-section").style.display = "none";
+            document.querySelector(".starter-section").style.display = "block";
+        });
+    });
+
+    replay.addEventListener("click", (e) => { 
+        resetGameBoardObject();
+        resetGameBoardCellContent();
+        document.querySelector("#end-dialog").close();
+    })
+})();
+
+function winnerHandler(winnerSign) { 
+    document.querySelector("#end-dialog").showModal();
+    const dialogWinnerDisplay = document.querySelector(".dialog-header");
+
+    if (winnerSign == "X") { 
+        dialogWinnerDisplay.textContent = "Player" + startInteraction.getPlayerOneName() + "Won!";
+    } else { 
+        dialogWinnerDisplay.textContent = "Player" + startInteraction.getPlayerTwoName() + "Won!";
+    }
+}
+
+function drawHandler() { 
+    document.querySelector("#end-dialog").showModal();
+    document.querySelector(".dialog-header").textContent = "Game Drawn!";
 }
 
 
 
-
-
-// player names must have a significance (add on top whose turn it is (with some animation))
-
-// button to restart game 
-
-// show display at the end of game 
